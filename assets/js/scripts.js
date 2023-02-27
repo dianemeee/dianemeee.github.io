f0acc6ce538f706035647c42a15d04684a0ce01e
dianemeee <96942136+dianemeee@users.noreply.github.com>
2023-02-28 01:45:45 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   $(document).ready(function () {
     $('#sidebarToggle').click(function () {
       $('#sidebar').toggleClass('active');
@@ -7,14 +6,3 @@
     $('#closeSidebar').click(function () {
       $('#sidebar').removeClass('active');
     });
-=======
-  $(document).ready(function () {
-    $('#sidebarToggle').click(function () {
-      $('#sidebar').toggleClass('active');
-    });
-
-    $('#closeSidebar').click(function () {
-      $('#sidebar').removeClass('active');
-    });
->>>>>>> 08a333d08b46a3c2b838aa32a54226933699942c
-  });
